f89d3155fde0e472f2567bde71b73e564acb61d6
nikhil8545 <8545nikhil@gmail.com>
2020-01-28 09:24:19 +0530

    modified changes

--- CSE-B/config.php
+++ CSE-B/config.php
@@ -4,8 +4,8 @@ $userName = 'root';
 $password = '';
 $databaseName = 'cse2020';
     
-    
-if (mysqli_connect($hostName, $userName, $password, $databaseName)){
+ $conn = mysqli_connect($hostName, $userName, $password, $databaseName);
+if ($conn){
     echo 'Connection Successful!';
 }
 else{

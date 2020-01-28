e17428dcfe9f8f4260b01cdda11a111627ef1bc2
nikhil8545 <8545nikhil@gmail.com>
2020-01-28 09:48:04 +0530

    modified changes

--- CSE-B/phpInput.php
+++ CSE-B/phpInput.php
@@ -4,16 +4,11 @@ include('config.php');
 <?php
 if (isset($_POST['btn']))
 {
-echo "Dear, ".$_POST['name']." your details are".'<br>';	
-echo $_POST['id'].'<br>';
-//echo $_POST['name'].'<br>';
-$username = $_POST['name'].'<br>';
-//echo $_POST['age'].'<br>';
-$age = $_POST['age'].'<br>';
-//echo $_POST['contact'].'<br>';
-$contact = $_POST['contact'].'<br>';
-//echo $_POST['email'].'<br>';
-$email = $_POST['email'].'<br>';
+	echo "Dear, ".$_POST['name']." your details are".'<br>';	
+	echo $_POST['name'].'<br>';
+	echo $_POST['age'].'<br>';
+	echo $_POST['contact'].'<br>';
+	echo $_POST['email'].'<br>';
 }
 ?>
 <html>
@@ -23,15 +18,11 @@ $email = $_POST['email'].'<br>';
 
 	<body>
 	<form action="detail.php" method="post">
-		ID: <input name="id", type="text"
-		placeholder="Enter ID"><br>
-		Name: <input type="text" name="name"><br>
-		Age:<input type="text" name="age"
-		placeholder="Enter Age"><br>
-		Contact:<input type="text" name="contact"
-		placeholder="Enter contact"><br> 
-		email:<input type="text" name="email"
-		placeholder="Enter email"><br>
+	
+		Name: <input type="text" name="name" placeholder="Enter name"><br>
+		Age:<input type="text" name="age" placeholder="Enter Age"><br>
+		Contact:<input type="text" name="contact" placeholder="Enter contact"><br> 
+		email:<input type="text" name="email" placeholder="Enter email"><br>
 		<input type="submit" name='btn'>
 		</form>
 		</body>

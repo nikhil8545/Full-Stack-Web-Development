fc3ed105dd6126f3720e245144453fced45f065f
nikhil8545 <8545nikhil@gmail.com>
2020-01-28 11:57:57 +0530

    modified changes

---
+++ CSE-B/input.php
@@ -0,0 +1,29 @@
+<?php
+include('config.php');
+?>
+<?php
+if (isset($_POST['btn']))
+{
+	echo "Dear, ".$_POST['name']." your details are".'<br>';	
+	echo $_POST['name'].'<br>';
+	echo $_POST['age'].'<br>';
+	echo $_POST['contact'].'<br>';
+	echo $_POST['email'].'<br>';
+}
+?>
+<html>
+	<head>
+	<title>Input From</title>
+	</head>
+
+	<body>
+	<form action="addRecord.php" method="post">
+	
+		Name: <input type="text" name="name" placeholder="Enter name"><br>
+		Age:<input type="text" name="age" placeholder="Enter Age"><br>
+		Contact:<input type="text" name="contact" placeholder="Enter contact"><br> 
+		email:<input type="text" name="email" placeholder="Enter email"><br>
+		<input type="submit" name='btn'>
+		</form>
+		</body>
+	</html> 

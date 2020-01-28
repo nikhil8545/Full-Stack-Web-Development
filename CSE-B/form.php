43da561d5baedcc0d8ecd4a0b911f43755e3704f
nikhil8545 <8545nikhil@gmail.com>
2020-01-28 11:58:48 +0530

    html form practice

---
+++ CSE-B/form.php
@@ -0,0 +1,50 @@
+<html>
+<head>
+<title> Table </title>
+</head>
+<body>
+		<table>
+			<thead>
+			<tr>
+			<th>Name</th>
+			<th>Roll No.</th>
+			<th>Contact</th>
+			<th>Email</th>
+			</tr>
+			</thead>
+
+			<tbody>
+				<tr>
+				<th>Nikhil</th>
+				<th>10</th>
+				<th>85413552</th>
+				<th>nikhil@gmail.com</th>
+				</tr>
+
+				<tr>
+				<th>Mridul</th>
+				<th>20</th>
+				<th>265816549</th>
+				<th>mridul@gmail.com</th>
+				</tr>
+
+
+				<tr>
+				<th>Ishita</th>
+				<th>30</th>
+				<th>861564681</th>
+				<th>ishita@gmail.com</th>
+				</tr>
+
+
+				<tr>
+				<th>Abhinav</th>
+				<th>40</th>
+				<th>16816482</th>
+				<th>abhinav@gmail.com</th>
+				</tr>
+				
+			</tbody>
+		</table>
+</body>
+</html>

c20559ba28a45d48cab91df74a2eeb3d0b6875f9
nikhil8545 <8545nikhil@gmail.com>
2020-01-28 11:57:27 +0530

    modified changes

---
+++ CSE-B/details.php
@@ -0,0 +1,46 @@
+<html>
+<head>
+<title>Record</title>
+</head>
+<body>
+
+<?php
+include('config.php');
+?>
+
+<?php
+$sql = "SELECT * FROM `user`";
+$result = mysqli_query($conn, $sql);
+
+if($result->num_rows > 0){
+?>
+
+<table border="1">
+	<thead>
+		<tr>
+			<th>ID</th>
+			<th>Name</th>
+			<th>Age</th>
+			<th>Contact</th>
+			<th>Email</th>
+		</tr>
+	</thead>
+	<tbody>
+
+<?php
+	while($data = $result->fetch_assoc()){ ?>
+		<tr>
+			<td><?php echo $data['id'] ?></td>
+			<td><?php echo $data['name'] ?></td>
+			<td><?php echo $data['age'] ?></td>
+			<td><?php echo $data['contact'] ?></td>
+			<td><?php echo $data['email'] ?></td>
+		</tr>
+	<?php } ?>
+	</tbody>
+
+<?php } ?>
+</table>
+</body>
+</html>
+

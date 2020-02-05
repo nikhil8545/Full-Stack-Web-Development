33293cbab55731f19d8e60ea425c095e72c243b8
nikhil8545 <8545nikhil@gmail.com>
2020-02-05 14:08:20 +0530

    modified changes

--- CSE-B/addRecord.php
+++ CSE-B/addRecord.php
@@ -2,21 +2,18 @@
 include('config.php');
 ?>
 <?php
+
 if (isset($_POST['btn']))
 {
 echo "Dear, ".$_POST['name']." your are welcome".'<br>';	
-
 $username = $_POST['name'];
-
 $age = $_POST['age'];
-
 $contact = $_POST['contact'];
-
 $email = $_POST['email'];
-
 $sql = "INSERT INTO `user` (name, age, contact, email)
 VALUES ('$username','$age','$contact','$email')";
 mysqli_query($conn, $sql);
+header("location:details.php")
 }
  else{
  	echo "welcome guest";

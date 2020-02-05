00495852a0f52c742288bac1fe11234e0ddf729a
nikhil8545 <8545nikhil@gmail.com>
2020-02-05 14:33:09 +0530

    modified changes

--- CSE-B/details.php
+++ CSE-B/details.php
@@ -1,50 +1,45 @@
+<?php include('config.php'); ?>
+
 <html>
 <head>
-<title>Record</title>
+<title>Records</title>
 </head>
 <body>
 
 <?php
-include('config.php');
-?>
-
-<?php
-$sql = "SELECT * FROM `user`";
+$sql = "SELECT * FROM `users`";
 $result = mysqli_query($conn, $sql);
 
 if($result->num_rows > 0){
-?>
-
+?>   
 <table border="1">
-	<thead>
-		<tr>
-			<th>ID</th>
-			<th>Name</th>
-			<th>Age</th>
-			<th>Contact</th>
-			<th>Email</th>
-			<th>Edit</th>
-			<th>Delete</th>
-		</tr>
-	</thead>
-	<tbody>
-
-<?php
-	while($data = $result->fetch_assoc()){ ?>
-		<tr>
-			<td><?php echo $data['id'] ?></td>
-			<td><?php echo $data['name'] ?></td>
-			<td><?php echo $data['age'] ?></td>
-			<td><?php echo $data['contact'] ?></td>
-			<td><?php echo $data['email'] ?></td>
-			<td><a href="edit.php?id="<?php echo $data['id'];?>">Edit</a></td>
-			<td>Delete</td>
-		</tr>
-	<?php } ?>
-	</tbody>
-
+    <thead>
+        <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Age</th>
+            <th>Contact</th>
+            <th>E-Mail</th>
+            <th>Edit</th>
+            <th>Delete</th>
+        </tr>
+    </thead>
+    <tbody>
+    <?php    
+    while($data = $result->fetch_assoc()){ ?>
+    <tr>
+        <td><?php echo $data['id'];?></td>
+        <td><?php echo $data['name']; ?></td>
+        <td><?php echo $data['age']; ?></td>
+        <td><?php echo $data['contact']; ?></td>
+        <td><?php echo $data['email']; ?></td>
+        <td> <a href="edit.php?id=<?php echo $data['id'];?>">Edit</a> </td>
+        <td> <a href="delete.php?id=<?php echo $data['id'];?>">Delete</a></td>
+    </tr>
+    <?php } ?>
+    </tbody>
+    
 <?php } ?>
-</table>
-</body>
+</table>   
+</body>   
 </html>
-
